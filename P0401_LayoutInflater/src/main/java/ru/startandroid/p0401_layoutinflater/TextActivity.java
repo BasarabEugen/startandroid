c1bdd1856a2f1fa55a92babd986f2764340187fe
basarab.eugen <basarab.eugen@gmail.com>
2018-11-19 09:00:57 +0300

    startandroid

---
+++ P0401_LayoutInflater/src/main/java/ru/startandroid/p0401_layoutinflater/TextActivity.java
@@ -0,0 +1,13 @@
+package ru.startandroid.p0401_layoutinflater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text);
+    }
+}
