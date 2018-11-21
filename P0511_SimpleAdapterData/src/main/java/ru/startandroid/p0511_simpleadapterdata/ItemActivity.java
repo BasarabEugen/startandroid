a66f02f903bd745f3a6b1ee0ca781421912f854e
basarab.eugen <basarab.eugen@gmail.com>
2018-11-21 11:28:46 +0300

    startandroid

---
+++ P0511_SimpleAdapterData/src/main/java/ru/startandroid/p0511_simpleadapterdata/ItemActivity.java
@@ -0,0 +1,13 @@
+package ru.startandroid.p0511_simpleadapterdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item);
+    }
+}
